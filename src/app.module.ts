3aa20f1fd59cb4e3833a11323326a656b91243b6
Bartosz <barth.webdesign@gmail.com>
2024-08-29 12:35:41 +0200

    [Init]: modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
+import { MenuModule } from './menu/menu.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [],
+  imports: [OrdersModule, MenuModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
