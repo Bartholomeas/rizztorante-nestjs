25f87e54ada3619958abcf21f3e9a1e1f9c4a073
Bartosz <barth.webdesign@gmail.com>
2024-08-29 12:37:50 +0200

    [Chore]: prettier new config reformat

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { OrdersModule } from './orders/orders.module';
-import { MenuModule } from './menu/menu.module';
-import { CartModule } from './cart/cart.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { OrdersModule } from "./orders/orders.module";
+import { MenuModule } from "./menu/menu.module";
+import { CartModule } from "./cart/cart.module";
 
 @Module({
   imports: [OrdersModule, MenuModule, CartModule],
