b11006da3355e1ca5586b7bd41ba30c820cae2d2
Bartosz <barth.webdesign@gmail.com>
2024-09-02 09:08:40 +0200

    fix: test cases

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,6 +1,12 @@
-import { Test, TestingModule } from "@nestjs/testing";
+import type { TestingModule } from "@nestjs/testing";
+import { Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+
+import { Repository } from "typeorm";
 
 import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { User } from "./entity/user.entity";
 
 describe("AuthController", () => {
   let controller: AuthController;
@@ -8,6 +14,7 @@ describe("AuthController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [AuthService, { provide: getRepositoryToken(User), useClass: Repository }],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,13 +1,24 @@
-import { Test, TestingModule } from "@nestjs/testing";
+import type { TestingModule } from "@nestjs/testing";
+import { Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+
+import { Repository } from "typeorm";
 
 import { AuthService } from "./auth.service";
+import { User } from "./entity/user.entity";
 
 describe("AuthService", () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
