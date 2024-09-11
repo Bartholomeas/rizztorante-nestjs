ab31286489aa4b3857ef826fd2c2b5bf4eb93420
Bartosz <barth.webdesign@gmail.com>
2024-09-11 09:53:38 +0200

    fix: failing uploads testcases

--- src/uploads/uploads.service.spec.ts
+++ src/uploads/uploads.service.spec.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from "@nestjs/config";
 import type { TestingModule } from "@nestjs/testing";
 import { Test } from "@nestjs/testing";
 
@@ -8,7 +9,29 @@ describe("UploadsService", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UploadsService],
+      providers: [
+        UploadsService,
+        {
+          provide: ConfigService,
+          useValue: {
+            getOrThrow: jest.fn((key: string) => {
+              switch (key) {
+                case "S3_BUCKET_NAME":
+                  return "test-bucket";
+                case "S3_REGION":
+                  return "test-region";
+                case "S3_ACCESS_KEY":
+                  return "access-key";
+                case "S3_SECRET_ACCESS_KEY":
+                  return "secret-access-key";
+
+                default:
+                  throw new Error(`Unexpected config key: ${key}`);
+              }
+            }),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UploadsService>(UploadsService);
