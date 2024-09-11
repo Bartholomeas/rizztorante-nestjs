6aa50aecd204023c6e94739a05616e11f30eebb4
Bartosz <barth.webdesign@gmail.com>
2024-09-11 09:39:50 +0200

    feat: add uploading images module

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,5 +1,19 @@
-import { Controller, Get, Param, Post } from "@nestjs/common";
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import {
+  Controller,
+  Delete,
+  FileTypeValidator,
+  Get,
+  HttpException,
+  InternalServerErrorException,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { ApiBody, ApiConsumes, ApiOperation, ApiTags } from "@nestjs/swagger";
 
 import { UploadsService } from "./uploads.service";
 
@@ -11,12 +25,61 @@ export class UploadsController {
   @Get(":id")
   @ApiOperation({ summary: "Get file" })
   async getFile(@Param("id") id: string) {
-    return await this.service.getFile(id);
+    try {
+      return await this.service.getFile(id);
+    } catch (err) {
+      if (err instanceof HttpException) throw err;
+      else throw new InternalServerErrorException(err?.message);
+    }
   }
 
   @Post()
+  @UseInterceptors(FileInterceptor("file"))
   @ApiOperation({ summary: "Upload file" })
-  async uploadFile() {
-    return await this.service.uploadFile();
+  @ApiConsumes("multipart/form-data")
+  @ApiBody({
+    description: "File upload",
+    schema: {
+      type: "object",
+      properties: {
+        file: {
+          type: "string",
+          format: "binary",
+        },
+      },
+    },
+  })
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 1024 * 1024 * 5,
+            message: "File is too large. Max file size is 5MB",
+          }),
+          new FileTypeValidator({ fileType: /jpeg|jpg|png|webp/ }),
+        ],
+        fileIsRequired: true,
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    try {
+      return await this.service.uploadFile(file);
+    } catch (err) {
+      if (err instanceof HttpException) throw err;
+      else throw new InternalServerErrorException(err?.message);
+    }
+  }
+
+  @Delete(":id")
+  @ApiOperation({ summary: "Delete file" })
+  async deleteFile(@Param("id") id: string) {
+    try {
+      return await this.service.deleteFile(id);
+    } catch (err) {
+      if (err instanceof HttpException) throw err;
+      else throw new InternalServerErrorException(err?.message);
+    }
   }
 }
--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,9 +1,18 @@
 import { Module } from "@nestjs/common";
+import { ThrottlerModule } from "@nestjs/throttler";
 
 import { UploadsController } from "./uploads.controller";
 import { UploadsService } from "./uploads.service";
 
 @Module({
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 8000,
+        limit: 5,
+      },
+    ]),
+  ],
   controllers: [UploadsController],
   providers: [UploadsService],
 })
