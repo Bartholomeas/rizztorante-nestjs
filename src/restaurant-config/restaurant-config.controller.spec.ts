4e24b75903e7f5a827ae30b3a768ce3a1cec9b9d
Bartosz <barth.webdesign@gmail.com>
2024-09-12 15:35:19 +0200

    fix: failing testcases

--- src/restaurant-config/restaurant-config.controller.spec.ts
+++ src/restaurant-config/restaurant-config.controller.spec.ts
@@ -1,7 +1,14 @@
 import type { TestingModule } from "@nestjs/testing";
 import { Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
 
+import { Repository } from "typeorm";
+
+import { OperatingHours } from "./entities/operating-hours.entity";
+import { RestaurantConfig } from "./entities/restaurant-config.entity";
+import { SpecialDate } from "./entities/special-dates.entity";
 import { RestaurantConfigController } from "./restaurant-config.controller";
+import { RestaurantConfigService } from "./restaurant-config.service";
 
 describe("RestaurantConfigController", () => {
   let controller: RestaurantConfigController;
@@ -9,6 +16,21 @@ describe("RestaurantConfigController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RestaurantConfigController],
+      providers: [
+        RestaurantConfigService,
+        {
+          provide: getRepositoryToken(RestaurantConfig),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(OperatingHours),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(SpecialDate),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     controller = module.get<RestaurantConfigController>(RestaurantConfigController);
--- src/restaurant-config/restaurant-config.service.spec.ts
+++ src/restaurant-config/restaurant-config.service.spec.ts
@@ -1,6 +1,12 @@
 import type { TestingModule } from "@nestjs/testing";
 import { Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
 
+import { Repository } from "typeorm";
+
+import { OperatingHours } from "./entities/operating-hours.entity";
+import { RestaurantConfig } from "./entities/restaurant-config.entity";
+import { SpecialDate } from "./entities/special-dates.entity";
 import { RestaurantConfigService } from "./restaurant-config.service";
 
 describe("RestaurantConfigService", () => {
@@ -8,7 +14,21 @@ describe("RestaurantConfigService", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RestaurantConfigService],
+      providers: [
+        RestaurantConfigService,
+        {
+          provide: getRepositoryToken(RestaurantConfig),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(OperatingHours),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(SpecialDate),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<RestaurantConfigService>(RestaurantConfigService);
