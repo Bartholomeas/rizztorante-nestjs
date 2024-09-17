dffa8bb76ce77e02b5bb222e1713a5873cbce917
Bartholomeas <69473527+Bartholomeas@users.noreply.github.com>
2024-09-17 14:45:07 +0200

    feat: add seeding of restaurant config special dates (#33)
    Co-authored-by: Bartosz <barth.webdesign@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -51,7 +51,7 @@ import { OpinionsModule } from "./opinions/opinions.module";
     TypeOrmModule.forRoot({
       type: "postgres",
       host: process.env.DB_HOST ?? "localhost",
-      port: parseInt(process.env.DB_PORT ?? "5432"),
+      port: parseInt(process.env.DB_HOST_PORT ?? "5432"),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
