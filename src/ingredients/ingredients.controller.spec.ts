12b9768791b9057b09b10d64817c546742303493
Bartosz <barth.webdesign@gmail.com>
2024-09-19 10:09:04 +0200

    fix: failing testacases

--- src/ingredients/ingredients.controller.spec.ts
+++ src/ingredients/ingredients.controller.spec.ts
@@ -1,7 +1,12 @@
 import type { TestingModule } from "@nestjs/testing";
 import { Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
 
+import { Repository } from "typeorm";
+
+import { Ingredient } from "./entities/ingredient.entity";
 import { IngredientsController } from "./ingredients.controller";
+import { IngredientsService } from "./ingredients.service";
 
 describe("IngredientsController", () => {
   let controller: IngredientsController;
@@ -9,6 +14,13 @@ describe("IngredientsController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [IngredientsController],
+      providers: [
+        IngredientsService,
+        {
+          provide: getRepositoryToken(Ingredient),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     controller = module.get<IngredientsController>(IngredientsController);
--- src/ingredients/ingredients.service.spec.ts
+++ src/ingredients/ingredients.service.spec.ts
@@ -1,6 +1,10 @@
 import type { TestingModule } from "@nestjs/testing";
 import { Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
 
+import { Repository } from "typeorm";
+
+import { Ingredient } from "./entities/ingredient.entity";
 import { IngredientsService } from "./ingredients.service";
 
 describe("IngredientsService", () => {
@@ -8,7 +12,13 @@ describe("IngredientsService", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [IngredientsService],
+      providers: [
+        IngredientsService,
+        {
+          provide: getRepositoryToken(Ingredient),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<IngredientsService>(IngredientsService);
